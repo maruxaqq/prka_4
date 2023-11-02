e6e324628e4ba94d30c40cb7459f686567af24bd
maruxaqq <144634783+maruxaqq@users.noreply.github.com>
2023-11-02 09:08:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleAnimation() {
+    var object1 = document.querySelector('.__close');
+    var object2 = document.querySelector('.sidebar');
+    var object3 = document.querySelector('.icon');
+   
+    
+    object1.classList.toggle('active');
+    object2.classList.toggle('active');
+    object3.classList.toggle('active');
+    
+  }
+
+
